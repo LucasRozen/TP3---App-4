ac213add902d26e8f4db36cfb8cfec80e1a5e784
unknown <noepacio1625@gmail.com>
2024-04-25 10:56:10 -0300

    changes

---
+++ scraper.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.avisoPosteo = exports.descargarFotosPerfil = exports.extraerFotosPerfil = void 0;
+var urls = [];
+function extraerFotosPerfil(perfil) {
+    // Aquí implementarías la lógica para extraer las fotos del perfil dado
+    // Retornarías un arreglo de URLs de las imágenes
+    return [];
+}
+exports.extraerFotosPerfil = extraerFotosPerfil;
+function descargarFotosPerfil(urls) {
+    // Aquí implementarías la lógica para descargar las fotos dadas por las URLs
+}
+exports.descargarFotosPerfil = descargarFotosPerfil;
+function avisoPosteo(perfil) {
+    // Aquí implementarías la lógica para enviar un aviso cuando 
+    // se realice un nuevo posteo en el perfil dado
+}
+exports.avisoPosteo = avisoPosteo;
